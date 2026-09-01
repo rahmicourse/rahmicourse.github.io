2869b9b3039c51e7aec27a8df6a3134473caf290
Sukmawidianto <146106014+rahmicourse@users.noreply.github.com>
2026-09-01 22:29:56 +0700

    Update vite.config.ts

--- Lets-practice1/vite.config.ts
+++ Lets-practice1/vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, '.') },
+    alias: { 
+      '@': path.resolve(__dirname, './src') 
+    },
   },
 });
