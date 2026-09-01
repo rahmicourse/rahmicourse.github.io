451a51232df0d469dd588f2dfaa3652a95f8c60b
Sukmawidianto <146106014+rahmicourse@users.noreply.github.com>
2026-09-02 00:26:06 +0700

    Update vite.config.ts

--- Lets-practice1/vite.config.ts
+++ Lets-practice1/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Lets-practice1/', // <-- Ubah bagian ini agar sesuai dengan nama folder di GitHub
+  base: './', // <-- sudah diUBAH KE './' (Titik Garis Miring)
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: { 
