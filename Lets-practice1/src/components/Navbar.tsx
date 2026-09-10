ef9ec428acb4079c100cdb4b36fe7e9f21b8a140
Sukmawidianto <146106014+rahmicourse@users.noreply.github.com>
2026-09-10 17:42:27 +0700

    Update Navbar.tsx
    merubah di navbar : Developed by Rahmi Sukmawidianto

--- Lets-practice1/src/components/Navbar.tsx
+++ Lets-practice1/src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export const Navbar: React.FC<NavbarProps> = ({ currentMode, onSelectMode }) => 
               </span>
             </div>
             <p className="text-[11px] text-slate-500 font-medium hidden sm:block">
-              Introducing & Meeting New People
+              Developed by Rahmi Sukmawidianto
             </p>
           </div>
         </button>
